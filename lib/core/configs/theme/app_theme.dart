46afa6a5528c1c88207170c0b7aa804c96e9d504
sinzoli <513811093@qq.com>
2024-08-29 15:57:37 +0800

    splash

---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,35 @@
+import 'package:app/core/configs/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.lightBackground, // page color
+      brightness: Brightness.light,
+      fontFamily: 'Satoshi',
+      // ElevatedButton color
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primary,
+              textStyle: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)))));
+
+  static final darkTheme = ThemeData(
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.darkBackground,
+      brightness: Brightness.dark,
+      fontFamily: 'Satoshi',
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primary,
+              textStyle: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)))));
+}
